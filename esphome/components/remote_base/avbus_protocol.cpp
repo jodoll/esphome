7f91d78a81d97ca317e74709d5d44aae7e4d7a84
Johannes Doll <git@johannesdoll.com>
2022-11-08 23:15:50 +0100

    Use expect_pulse_with_gap for command

--- esphome/components/remote_base/avbus_protocol.cpp
+++ esphome/components/remote_base/avbus_protocol.cpp
@@ -58,9 +58,9 @@ optional<AvBusData> AvBusProtocol::decode(RemoteReceiveData src) {
   }
 
   for (uint8_t mask = 1; mask < 8; mask <<= 1) {
-    if (src.expect_item(BIT_ONE_US, BIT_ONE_SPACE_US)) {
+    if (src.expect_pulse_with_gap(BIT_ONE_US, BIT_ONE_SPACE_US)) {
       data.address |= mask;
-    } else if (src.expect_item(BIT_ZERO_US, BIT_ZERO_SPACE_US)) {
+    } else if (src.expect_pulse_with_gap(BIT_ZERO_US, BIT_ZERO_SPACE_US)) {
       data.address &= ~mask;
     } else {
       return {};
